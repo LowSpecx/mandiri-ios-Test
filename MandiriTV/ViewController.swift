423243bb4dd5edd5226e754e77daef66927385da
mauricetin <mauricetin.916@gmail.com>
2023-01-18 14:14:23 +0700

    Initial Commit

---
+++ MandiriTV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MandiriTV
+//
+//  Created by Maurice Tin on 18/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
