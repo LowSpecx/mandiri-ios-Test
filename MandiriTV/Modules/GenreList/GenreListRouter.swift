4400d46e9e3c2ed8cf0de2590ec79f6a32236292
mauricetin <mauricetin.916@gmail.com>
2023-01-20 09:32:59 +0700

    add Genre list router input output

--- MandiriTV/Modules/GenreList/GenreListRouter.swift
+++ MandiriTV/Modules/GenreList/GenreListRouter.swift
@@ -6,7 +6,33 @@
 //
 
 import Foundation
+import UIKit
 
 struct GenreListRouterInput{
+    private func view()->GenreViewController{
+        let view = GenreViewController()
+        return view
+    }
     
+    public func push(from: Viewable){
+        let view = self.view()
+        from.push(view, animated: true)
+    }
+    
+    public func present(from: Viewable){
+        let nav = UINavigationController(rootViewController: view())
+        from.present(nav, animated: true)
+    }
+}
+
+final class GenreListRouterOutput: Routerable{
+    private(set) weak var view: Viewable!
+    
+    init(view: Viewable!) {
+        self.view = view
+    }
+    
+    func transitionDetail(){
+        GenreListRouterInput().push(from: view)
+    }
 }
